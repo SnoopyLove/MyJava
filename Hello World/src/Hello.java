995f3dd25ba212c40b271b3639724da1842f5d5f
Mr. A <Mr. A@192.168.1.7>
2022-03-05 09:02:01 +0530

    First Commit

---
+++ Hello World/src/Hello.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+import org.studyeasy.IsPrime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// OM SAI RAM
+ ArrayList<String> List= new ArrayList<>();
+ Stack<String> List1= new Stack<>();
+ String a = "z";
+List.add("v");
+System.out.println(List);
+int i =a.compareTo("p");
+System.out.println(i);
+System.out.println("My Jenkins First Program");
+
+	}
+
+}
---
+++ Hello World/src/org/studyeasy/Car.java
@@ -0,0 +1,26 @@
+package org.studyeasy;
+
+public class Car {
+Animal a=new Animal ();
+
+	public String getDoors() {
+		return Doors;
+	}
+	public void setDoors(String doors) {
+		Doors = doors;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	private String Doors;
+	private int speed;
+	@Override
+	public String toString() {
+		return "Car [Doors=" + Doors + ", speed=" + speed + ", getDoors()=" + getDoors() + ", getSpeed()=" + getSpeed()
+				+ "]";
+	}
+	
+}
